1bef096fb13543c2689be6a66d50b07ca6491ca0
kimanzialu <k.imanzi@alustudent.com>
2025-07-04 07:59:00 -0700

    commit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,11 +6,19 @@ import requests
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'MyBot/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    headers = {'User-Agent': 'python:top_ten:v1.0 (by /u/username)'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            print(None)
+            return
+        
+        data = response.json()
+        posts = data['data']['children']
+        
+        for post in posts:
+            print(post['data']['title'])
+            
+    except Exception:
         print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
